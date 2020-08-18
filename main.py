07c24c12eeb0809e011c9e5bce5582156c09d8f6
jaeden Haste-pollard <jaedenh@wainuiomatahigh.school.nz>
2020-08-18 23:24:09 +0000

    adding comments

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ from tkinter import *
 import os 
 root = Tk()
 
+#opening the guessing game
 def Guessing_game():
   os.system('python Games/Guessing.py')
 
+#opening the snake game 
+def Snake_game(): 
+  os.system('python Games/guessing.py')
 
+
+
+#the buttons for the game 
 Button_Game1 = Button(root, text='Guessing game', command=Guessing_game )
-Button_Game2 = Button(root, text='snake', )
+Button_Game2 = Button(root, text='snake game', command=Snake_game )
 Button_Game3 = Button(root, text='3', )
 
-
+#packing the buttons in the the gui 
 Button_Game1.pack()
 Button_Game2.pack()
 Button_Game3.pack()
