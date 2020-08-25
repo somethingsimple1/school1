8f12a1294e9a7d0baf5b7df1433be7c84c86c742
jaeden Haste-pollard <jaedenh@wainuiomatahigh.school.nz>
2020-08-25 09:48:00 +0000

    added comments

--- main.py
+++ main.py
@@ -12,25 +12,31 @@ if __name__ == "__main__":
   root.title("My Games")
 
   
-  #making the frame 
+  #making the frame called "label" 
   label = tk.Frame(root, width=35, height=240, padx=constants.layout["pad_x"], pady=constants.layout["pad_y"])
 
+  #making the label and adding them to the frame 
   tk.Label(label, text="Pick the game you want to play.").grid()
 
 
-
+  #adding the fram called "label" to the grid 
   label.grid(column=0, row=0)
 
     
 
-  #the buttons for the game 
+  #making the frame calleed "buttons"
   buttons = tk.Frame(root, width=35, height=240)
 
+  #making the buttons and adding them to the frame 
   tk.Button(buttons, text='Guessing game').grid()
   tk.Button(buttons, text='snake game').grid()
   tk.Button(buttons, text='Flappy bird').grid()
 
+  #addig the frame called "buttons" to the grid
   buttons.grid(column=0, row=1)
+
+
+  
 
   root.mainloop() 
 
